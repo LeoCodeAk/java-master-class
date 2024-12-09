591ec0b21d9d78e824e162034344c01b00e37d11
LeoCodeAk <akash.bhattarai2@gmail.com>
2024-12-09 10:00:18 -0500

    temp to store var

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x = 5;
+        int y = 10;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        int temp = x; //temp = 10
+        x = y; // x = 10
+        y = temp; // y = 10
+
+        System.out.println(x);
+        System.out.println(y);
     }
 }
